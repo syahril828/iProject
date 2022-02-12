3a67a6f5a06a7beba06a7270c490f64684e4e962
syahril828 <mr.muhammadsyahril@gmail.com>
2022-02-12 09:32:06 +0900

    Membuat proyek iSkripsi

---
+++ SwiftUI/iSkripsi/iSkripsi/iSkripsi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSkripsi
+//
+//  Created by Muhammad Syahril on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
